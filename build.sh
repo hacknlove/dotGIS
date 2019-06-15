baa3b92f807b49d30aa58813f82c228ba926826a
antonio <antonio@pykiss.com>
2019-06-15 10:56:09 +0200

    last steps

--- build.sh
+++ build.sh
@@ -2,13 +2,14 @@ npm run build
 
 VERSION=$(node -pe "require('./package.json').version")
 NAME=$(node -pe "require('./package.json').name")
+DOCKER_HUB_USER=$(node -pe "require('./package.json').dockerHubUser")
 
 cp Dockerfile build/
 
-docker build build/ -t hacknlove/$NAME:$VERSION
+docker build build/ -t ${DOCKER_HUB_USER}/$NAME:$VERSION
 
 rm -rf build
 
-docker push hacknlove/$NAME:$VERSION
-docker tag hacknlove/$NAME:$VERSION hacknlove/$NAME:latest
-docker push hacknlove/$NAME:latest
+docker push ${DOCKER_HUB_USER}/$NAME:$VERSION
+docker tag ${DOCKER_HUB_USER}/$NAME:$VERSION ${DOCKER_HUB_USER}/$NAME:latest
+docker push ${DOCKER_HUB_USER}/$NAME:latest
